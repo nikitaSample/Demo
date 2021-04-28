65f3908877e4909ca079c36ddde93d9e4cef3a0e
nikita <nikita@localhost>
2021-04-28 22:48:24 +0530

    specified the path for creating extent report file.

--- src/main/java/utilities/ReportSetUp.java
+++ src/main/java/utilities/ReportSetUp.java
@@ -1,11 +1,14 @@
 package utilities;
 
+import java.io.File;
+
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 
 public class ReportSetUp {
 
-		private static ExtentReports report = new ExtentReports(Helpers.readConfigFile("REPORTS_PATH"), true);
+		private static ExtentReports report = new ExtentReports(System.getProperty("user.dir")+File.separator+Helpers.readConfigFile("REPORTS_PATH"), true);
+		
 		private static ExtentTest test;
 		
 		private ReportSetUp() {
