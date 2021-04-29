f922b9122f8f73979617ae5d950831ce824688ee
nikita <nikita@localhost>
2021-04-29 17:56:08 +0530

    modified the naming convention of methods to make it more logical.

--- src/main/java/appSpecificLibrary/BBCWeatherPageMethods.java
+++ src/main/java/appSpecificLibrary/BBCWeatherPageMethods.java
@@ -10,22 +10,12 @@ public class BBCWeatherPageMethods extends BBCWeatherPageObjects{
 		CommonActionsUI obj = new CommonActionsUI();
 		int timeOut = 10;
 		
-		public Map<String,String> getWeatherDetails(String cityName) throws Exception{
-			
-			Map<String,String> uiData = new HashMap<String,String>();
+		public void searchCityByName(String cityName) throws Exception{			
 			
 			obj.Click(getSearchCityField());
 			obj.inputText(getSearchCityField(), cityName);
 			obj.waitForElementList(getListCities(), timeOut);
 			obj.selectFromDynamicDropdown(getListCities(), cityName);
-			obj.waitForElementToBeVisible(getTempCard(), timeOut);
 			
-			try {
-				uiData.put("High", obj.getText(getHighTemp()));
-				uiData.put("Low", obj.getText(getLowTemp()));
-			}catch(Exception e) {
-				throw new Exception("Temperature not displayed on site.");
-			}
-			return uiData;
 		}
 }
--- src/main/java/appSpecificLibrary/BBCWeatherPageObjects.java
+++ src/main/java/appSpecificLibrary/BBCWeatherPageObjects.java
@@ -40,24 +40,4 @@ public class BBCWeatherPageObjects {
 		public WebElement getSearchBtn() {
 			return searchBtn;
 		}
-		
-		@FindBy(xpath = "//a[@id='daylink-0']")
-		private WebElement tempCard;
-		public WebElement getTempCard() {
-			return tempCard;
-		}
-		
-		@FindBy(xpath = "//a[@id='daylink-0']//span[@class='wr-day-temperature__high-value']//span[@class='wr-value--temperature--f']")
-		private WebElement highTemp;
-		public WebElement getHighTemp() {
-			return highTemp;
-		}
-		
-		@FindBy(xpath = "//a[@id='daylink-0']//span[@class='wr-day-temperature__low-value']//span[@class='wr-value--temperature--f']")
-		private WebElement lowTemp;
-		public WebElement getLowTemp() {
-			return lowTemp;
-		}
-		
-		
 }
